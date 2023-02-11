229a9c1d610034235d9360efdca0e909c9d9c474
Lethe1999 <lucaswu1999@gmail.com>
2023-02-10 23:16:14 -0500

    Assignment4

--- src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
+++ src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
@@ -1,4 +1,40 @@
 package edu.neu.coe.info6205.union_find;
 
+import java.util.Random;
+
 public class HWQUPC_Solution {
+
+    /**
+     * Return the number of connections
+     *
+     * @param n
+     * @return cnt_connection
+     */
+    public static int count(int n) {
+        Random random = new Random();
+
+        UF_HWQUPC uf = new UF_HWQUPC(n, true);  // Create an object
+
+        int cnt_connection = 0;     // Number of pairs
+        while (uf.components() > 1) {
+            uf.connect(random.nextInt(n), random.nextInt(n));   // Generate a new pair randomly
+            cnt_connection++;
+        }
+
+        return cnt_connection;
+    }
+
+    public static void main(String[] args) {
+        for (int n = 10; n < 5000; n += 100) {
+            System.out.print("n = " + n);
+//            System.out.println("\t\tm = " + count(n));
+
+            // Calculate the average m
+            int m = 0;
+            for (int j = 0; j < 10; j++) {
+                m += count(n);
+            }
+            System.out.println("\t\tm = " + m / 10);
+        }
+    }
 }
--- src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
@@ -82,6 +82,12 @@ public class UF_HWQUPC implements UF {
         validate(p);
         int root = p;
         // FIXME
+        while (root != parent[root]) {
+            if (pathCompression) {
+                doPathCompression(root);
+            }
+            root = parent[root];
+        }
         // END 
         return root;
     }
@@ -170,6 +176,14 @@ public class UF_HWQUPC implements UF {
 
     private void mergeComponents(int i, int j) {
         // FIXME make shorter root point to taller one
+        if (i == j) return;
+        else if (height[i] < height[j]) {
+            parent[i] = j;
+            height[j] += height[i];
+        } else {
+            parent[j] = i;
+            height[i] += height[j];
+        }
         // END 
     }
 
@@ -178,6 +192,7 @@ public class UF_HWQUPC implements UF {
      */
     private void doPathCompression(int i) {
         // FIXME update parent to value of grandparent
+        parent[i] = parent[parent[i]];
         // END 
     }
 }
